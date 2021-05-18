5f866890aaf0b454f783a335edd83e2aaaf7f44f
Max Streese <max.streese@smarttra.de>
2021-05-18 09:55:21 +0200

    Make a few improvements

--- src/test/scala/com/streese/interview4s/DummySpec.scala
+++ src/test/scala/com/streese/interview4s/DummySpec.scala
@@ -3,4 +3,8 @@ package com.streese.interview4s
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-class DummySpec extends AnyFreeSpec with Matchers {}
+class DummySpec extends AnyFreeSpec with Matchers {
+  "This dummy test should work" in {
+    1+1 shouldBe 2
+  }
+}
